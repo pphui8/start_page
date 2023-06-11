fcdb16e97fe84d3c2bf9743dd60af9ff7497c015
金晖 <55843852+pphui8@users.noreply.github.com>
2023-06-11 22:00:43 +0800

    add search engine change

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const searchEngineSelect = document.getElementById('search-engine');
+const searchEngineSelect = document.getElementById("engine-icon-container");
 const searchInput = document.getElementById('search-input');
+
+const googleIcon = document.getElementById('googleIcon');
+const bingIcon = document.getElementById('bingIcon');
+
+let searchEngine = 'bing';
 
 searchInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        // const searchEngine = searchEngineSelect.value;
+        console.log(searchEngine);
         const searchTerm = encodeURIComponent(searchInput.value);
-
-        // const searchURL = searchEngine === 'google'
-            // ? `https://www.google.com/search?q=${searchTerm}`
-            // : `https://www.bing.com/search?q=${searchTerm}`;
-
         const searchURL = `https://www.google.com/search?q=${searchTerm}`;
 
         window.location.href = searchURL;
     }
 });
+
+searchEngineSelect.addEventListener('click', () => {
+        if(searchEngine === 'bing') {
+            searchEngine = 'google';
+            googleIcon.setAttribute('style', 'display: inline-block');
+            bingIcon.setAttribute('style', 'display: none');
+        } else {
+            searchEngine = 'bing';
+            googleIcon.setAttribute("style", "display: none");
+            bingIcon.setAttribute('style', 'display: inline-block');
+        }
+    }
+);
