410ef0bae4ad4029da6c82a7073a34e1137ff0bb
金晖 <55843852+pphui8@users.noreply.github.com>
2023-06-12 12:31:06 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const searchContainer = document.getElementById('search-container');
 const searchEngineSelect = document.getElementById("engine-icon-container");
 const searchInput = document.getElementById('search-input');
 
 const googleIcon = document.getElementById('googleIcon');
 const bingIcon = document.getElementById('bingIcon');
+
+const historyContainer = document.getElementById('history-container');
 
 let searchEngine = 'bing';
 
@@ -14,6 +17,38 @@ searchInput.addEventListener('keydown', (event) => {
 
         window.location.href = searchURL;
     }
+});
+
+searchInput.addEventListener('keyup', () => {
+        if(searchInput.value !== '' && searchInput.value !== null) {
+            historyContainer.setAttribute('style', 'visibility: visible; opacity: 1');
+
+            // insert history item
+
+
+        } else if (event.key === "Backspace" && searchInput.value === "") {
+            historyContainer.setAttribute(
+              "style",
+              "visibility: hidden; opacity: 0"
+            );
+        }
+    }
+);
+
+document.addEventListener('click', (event) => {
+    historyContainer.setAttribute(
+        "style",
+        "visibility: hidden; opacity: 0"
+    );
+    searchInput.value = '';
+});
+
+historyContainer.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+searchContainer.addEventListener('click', (event) => {
+    event.stopPropagation();
 });
 
 searchEngineSelect.addEventListener('click', () => {
